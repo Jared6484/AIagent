0b23250f6d1cf9216a308f17f7cc5562c993def6
jared6484 <jared.6484@gmail.com>
2025-08-21 08:45:32 -0400

    updated functions and schema

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.write_file import write_file, schema_write_file
+from functions.run_python import run_python_file, schema_run_python_file
 
 def main():
     print("Hello from aiagent!")
@@ -18,11 +22,32 @@ def main():
     messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
+    available_functions = types.Tool(function_declarations=[schema_get_files_info, schema_get_file_content,schema_run_python_file, schema_write_file])
+
+    
+    #system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+    - Read file contents
+    - Execute Python files with optional arguments
+    - Write or overwrite files
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
 
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions],system_instruction=system_prompt),
     )
+
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
     print(response.text)
     if len(sys.argv) >2 and sys.argv[2] == "--verbose":
         print(f"User prompt: {user_prompt}")
--- tests.py
+++ tests.py
@@ -1,18 +1,40 @@
-from functions.get_file_content import get_file_content
-
-content1 = get_file_content("calculator", "main.py")
-print(content1)
-content2 = get_file_content("calculator", "pkg/calculator.py")
-print(content2)
-content3 = get_file_content("calculator", "/bin/cat") #(this should return an error string)
-print(content3)
-content4 = get_file_content("calculator", "pkg/does_not_exist.py") #(this should return an error string)
-print(content4)
+#from functions.get_file_content import get_file_content
+# from functions.write_file import write_file
+from functions.run_python import run_python_file
 
 
 
 
+result1 = run_python_file("calculator", "main.py") #(should print the calculator's usage instructions)
+print(result1)
+result2 = run_python_file("calculator", "main.py", ["3 + 5"]) #(should run the calculator... which gives a kinda nasty rendered result)
+print(result2)
+result3 = run_python_file("calculator", "tests.py")
+print(result3)
+result4 = run_python_file("calculator", "../main.py") #(this should return an error)
+print(result4)
+result5 = run_python_file("calculator", "nonexistent.py") #(this should return an error)
+print(result5)
 
+
+
+# result1 = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+# print(result1)
+# result2 = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+# print(result2)
+# result3 = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+# print(result3)
+
+
+
+# content1 = get_file_content("calculator", "main.py")
+# print(content1)
+# content2 = get_file_content("calculator", "pkg/calculator.py")
+# print(content2)
+# content3 = get_file_content("calculator", "/bin/cat") #(this should return an error string)
+# print(content3)
+# content4 = get_file_content("calculator", "pkg/does_not_exist.py") #(this should return an error string)
+# print(content4)
 
 
 # content = get_file_content("calculator", "lorem.txt")
